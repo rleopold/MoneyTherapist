034c19be1b0822636f3500c5e0f5d7d6c9e0f941
Richard J. Leopold <rjleopold@gmail.com>
2015-03-01 16:21:22 -0600

    initial commit

---
+++ MoneyTherapist/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoneyTherapist.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Mortgage()
+        {
+            return View();
+        }
+
+        public ActionResult CarLoan()
+        {
+            return View();
+        }
+
+        public ActionResult Shoes()
+        {
+            return View();
+        }
+
+    }
+}

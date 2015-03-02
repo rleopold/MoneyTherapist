a16a65124db47903a94a95903b290d979e0809c9
Richard Leopold <C101169@USGVLH4P4NX1.babgsetc.pvt>
2015-03-02 12:46:32 -0600

    added js file

---
+++ MoneyTherapist/Scripts/calculator.js
@@ -0,0 +1,14 @@
+﻿
+// return monthly mortgage payment
+// amount - mortgage amount in dollars
+// term - term in years
+// rate - annual interest rate of loan
+function mortgageMonthlyPayment(amount, term, rate) {
+
+    var n = terms * 12;
+    var i = (rate / 100) / 12;
+
+    var payment = amount * ((i * Math.pow(1 + i, n)) / ((Math.pow(1 + i, n) - 1)));
+
+    return payment;
+}
